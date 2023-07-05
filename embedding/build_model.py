45f86b6f51b50b359218a0c6ea89515db3c7f8b6
PierreFts <pierre.fontesse@student-cs.fr>
2023-07-05 19:44:38 +0200

    first step

---
+++ embedding/build_model.py
@@ -0,0 +1,31 @@
+from sentence_transformers import SentenceTransformer
+
+
+if __name__ == '__main__':
+
+    #file for artifacts
+    model_artifacts_folder = "model_artifacts"
+
+    #the sentences we would like to encode
+    sentences = ["This is a coding assignment", "We are in the first task."]
+
+    #choice of our model, here we took a sentence transformer to have a vector per input sentence instead of one vector per token (so more than one per word...)
+    model = SentenceTransformer('AI-Growth-Lab/PatentSBERTa')
+
+    #its artifacts
+    model.save(model_artifacts_folder)
+
+    #Sentences are encoded by calling model.encode()
+    embeddings = model.encode(sentences)
+
+    #checking the embeddings
+    shape = embeddings.shape
+    if(shape[0]!=len(sentences)):
+        print("problem : not one vector per input")
+    else:
+        print("no issue")
+    # #further prints to see the embeddings, from the doc of hugging face
+    # for sentence, embedding in zip(sentences, embeddings):
+    #     print("Sentence:", sentence)
+    #     print("Embedding:", embedding)
+    #     print("")
